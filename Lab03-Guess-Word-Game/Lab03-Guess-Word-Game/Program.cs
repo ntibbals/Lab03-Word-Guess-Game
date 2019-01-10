b0b08ca7f30b540321ebba2c1568191cfca6e418
ntibbals <ntibbals@gmail.com>
2019-01-10 09:52:07 -0800

    setup menu

--- Lab03-Guess-Word-Game/Lab03-Guess-Word-Game/Program.cs
+++ Lab03-Guess-Word-Game/Lab03-Guess-Word-Game/Program.cs
@@ -2,11 +2,67 @@
 
 namespace Lab03_Guess_Word_Game
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool displayMenu = true;
+            while(displayMenu)
+            {
+                Console.WriteLine("Do you want to play a game? y/n");
+                string start = Console.ReadLine();
+                if (start == "y")
+                {
+                    MainMenu();
+                }
+                else if (start == "n")
+                {
+                    Environment.Exit(0);
+                }
+            }
+        }
+
+        public static void MainMenu()
+        {
+            Console.Clear();
+            try
+            {
+                Console.Clear();
+                Console.WriteLine("Welcome to the Word Guess Game!");
+                Console.WriteLine("1) Start game");
+                Console.WriteLine("2) Admin");
+                Console.WriteLine("3) Exit");
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        StartGame();
+                        break;
+
+                }
+            }
+            catch (ArgumentNullException e) //catch null exception
+            {
+                Console.WriteLine($"You did not enter anything. Please try again.");
+                Console.ReadLine();
+            }
+            catch (FormatException e) // catch format exception
+            {
+                Console.WriteLine($"Unable to read format. Please try agian.");
+                Console.ReadLine();
+            }
+            catch (Exception e) // catch all exception(general)
+            {
+                Console.WriteLine($"You've hit the following exception: {e.Message}.");
+                Console.ReadLine();
+            }
+
+        }
+
+        static void StartGame()
+        {
+            Console.WriteLine("Game Running.");
+            Console.ReadLine();
         }
     }
 }
