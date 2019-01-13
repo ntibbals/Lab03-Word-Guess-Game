e2e11ab2cfdc06f3a8c3886b12d6f93eae94d5a5
ntibbals <ntibbals@gmail.com>
2019-01-12 20:19:14 -0800

    created second test for deletion, success

--- Lab03-Guess-Word-Game/Guess-Word-Test/UnitTest1.cs
+++ Lab03-Guess-Word-Game/Guess-Word-Test/UnitTest1.cs
@@ -7,11 +7,22 @@ namespace Guess_Word_Test
     public class UnitTest1
     {
         [Fact]
-        public void TestUpdate()
+        public void TestUpdateWord()
         {
+            /// test that given a new word, will add to existing file
             string testHouse = "TESTHOUSE";
             string[] expectedResult = { "LANNISTER", "BARATHEON", "GREYJOY", "STARK", "TYRELL", "BOLTON", "TARGARYEN", "TESTHOUSE" };
             Assert.Equal(expectedResult, Program.AddHouse(testHouse));
+        }
+
+        [Fact]
+        public void TestDeleteWord()
+        {
+            /// test that given a word to delete, will delete and return updated file
+            string testHouse = "TARGARYEN";
+            string testAnswers = "../../../../../GameWords.txt";
+            string[] expectedResult = { "LANNISTER", "BARATHEON", "GREYJOY", "STARK", "TYRELL", "BOLTON" };
+            Assert.Equal(expectedResult, Program.DeleteHouse(testHouse, testAnswers));
         }
     }
 }
