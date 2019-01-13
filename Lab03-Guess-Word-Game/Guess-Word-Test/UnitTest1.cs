109677bea0795d9598de7813edbc7a6455e7c7c4
ntibbals <ntibbals@gmail.com>
2019-01-12 21:08:02 -0800

    Tests for if a letter exists and if a letter does not exists passed after refactor

--- Lab03-Guess-Word-Game/Guess-Word-Test/UnitTest1.cs
+++ Lab03-Guess-Word-Game/Guess-Word-Test/UnitTest1.cs
@@ -39,11 +39,23 @@ namespace Guess_Word_Test
         [Fact]
         public void TestLetterExists()
         {
-            /// test that given a instantiated file, retrieval of words is possible
-            string testAnswers = "../../../../../GameWords.txt";
-            Program.CreateFile(testAnswers);
-            string[] expectedResult = { "LANNISTER", "BARATHEON", "GREYJOY", "STARK", "TYRELL", "BOLTON", "TARGARYEN" };
-            Assert.Equal(expectedResult, Program.ReadFileWords(testAnswers));
+            /// test that given a letter, will alter array if exists
+            string testKey = "NATE";
+            string testGuess = "A";
+            string[] testArray = { "_", "_", "_", "_" };
+            string[] expectedResult = { "_", "A", "_", "_" };
+            Assert.Equal(expectedResult, Program.ValidateGuess(testKey, testGuess, testArray));
+        }
+
+        [Fact]
+        public void TestNoLetterExists()
+        {
+            /// test that given a letter, will not alter array if it does not exists
+            string testKey = "NATE";
+            string testGuess = "Z";
+            string[] testArray = { "_", "_", "_", "_" };
+            string[] expectedResult = { "_", "_", "_", "_" };
+            Assert.Equal(expectedResult, Program.ValidateGuess(testKey, testGuess, testArray));
         }
     }
 }
